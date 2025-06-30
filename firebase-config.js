882d3b38b7491e6e271bcb2417599418c0c748e0
GerryPoz <pliplomail@gmail.com>
2025-06-30 14:15:46 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "TUO_API_KEY",
+  authDomain: "TUO_DOMINIO.firebaseapp.com",
+  databaseURL: "https://TUO_DATABASE.firebaseio.com",
+  projectId: "TUO_PROJECT_ID",
+  storageBucket: "TUO_BUCKET.appspot.com",
+  messagingSenderId: "TUO_SENDER_ID",
+  appId: "TUO_APP_ID"
+};
