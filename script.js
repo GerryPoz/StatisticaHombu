2351f9038110737f5dfd7d3641a1b33da96d59fc
GerryPoz <pliplomail@gmail.com>
2025-07-06 13:21:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -80,49 +80,45 @@ document.getElementById("dati-form").addEventListener("submit", (e) => {
     gruppo: data.gruppo,
     zadankai: {
       membri: {
-        U: +data.zadankai_m_u,
-        D: +data.zadankai_m_d,
-        GU: +data.zadankai_m_gu,
-        GD: +data.zadankai_m_gd,
-        FUT: +data.zadankai_m_fut,
-        STU: +data.zadankai_m_stu
+        U: +data.zadankai_m_u || 0,
+        D: +data.zadankai_m_d || 0,
+        GU: +data.zadankai_m_gu || 0,
+        GD: +data.zadankai_m_gd || 0,
+        FUT: +data.zadankai_m_fut || 0,
+        STU: +data.zadankai_m_stu || 0
       },
       simpatizzanti: {
-        U: +data.zadankai_s_u,
-        D: +data.zadankai_s_d,
-        GU: +data.zadankai_s_gu,
-        GD: +data.zadankai_s_gd,
-        FUT: +data.zadankai_s_fut,
-        STU: +data.zadankai_s_stu
+        U: +data.zadankai_s_u || 0,
+        D: +data.zadankai_s_d || 0,
+        GU: +data.zadankai_s_gu || 0,
+        GD: +data.zadankai_s_gd || 0,
+        FUT: +data.zadankai_s_fut || 0,
+        STU: +data.zadankai_s_stu || 0
       },
       ospiti: {
-        U: +data.zadankai_o_u,
-        D: +data.zadankai_o_d,
-        GU: +data.zadankai_o_gu,
-        GD: +data.zadankai_o_gd
+        U: +data.zadankai_o_u || 0,
+        D: +data.zadankai_o_d || 0,
+        GU: +data.zadankai_o_gu || 0,
+        GD: +data.zadankai_o_gd || 0
       }
     },
     praticanti: {
       membri: {
-        U: +data.praticanti_m_u,
-        D: +data.praticanti_m_d,
-        GU: +data.praticanti_m_gu,
-        GD: +data.praticanti_m_gd
+        U: +data.praticanti_m_u || 0,
+        D: +data.praticanti_m_d || 0,
+        GU: +data.praticanti_m_gu || 0,
+        GD: +data.praticanti_m_gd || 0
       },
       simpatizzanti: {
-        U: +data.praticanti_s_u,
-        D: +data.praticanti_s_d,
-        GU: +data.praticanti_s_gu,
-        GD: +data.praticanti_s_gd
+        U: +data.praticanti_s_u || 0,
+        D: +data.praticanti_s_d || 0,
+        GU: +data.praticanti_s_gu || 0,
+        GD: +data.praticanti_s_gd || 0
       }
     }
   };
 
   set(ref(db, `zadankai/${key}`), payload)
-  .then(() => {
-    document.getElementById("messaggio-successo").style.display = "block";
-    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-  })
-  .catch(err => alert("❌ Errore nel salvataggio: " + err.message));
-
-});
+    .then(() => {
+      document.getElementById("messaggio-successo").style.display = "block";
+      window.scrollTo({ top
