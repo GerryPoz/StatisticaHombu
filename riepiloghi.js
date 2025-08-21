e4f744b05be689649598d2e7e9e03129dd4143cc
GerryPoz <pliplomail@gmail.com>
2025-08-22 01:01:17 +0200

    Update riepiloghi.js

--- riepiloghi.js
+++ riepiloghi.js
@@ -763,7 +763,7 @@ function esportaPdf() {
             //     yPosition = 20;
             // }
             
-            doc.setFontSize(14);
+            doc.setFontSize(12);
             doc.text(`Settore: ${settore}`, 20, yPosition);
             yPosition += 8;
             
@@ -839,7 +839,7 @@ function esportaPdf() {
                 head: intestazioniSettore,
                 body: righeTabSettore,
                 startY: yPosition,
-                styles: { fontSize: 9 },
+                styles: { fontSize: 7 },
                 headStyles: { fillColor: [255, 193, 7] },
                 columnStyles: {
                     6: { fontStyle: 'bold' }, // Somma
