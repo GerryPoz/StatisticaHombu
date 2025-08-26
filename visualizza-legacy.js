dfd33e541433f3a3b9235469e0bdb9e2ffee986b
GerryPoz <pliplomail@gmail.com>
2025-08-26 18:28:11 +0200

    Update visualizza-legacy.js

--- visualizza-legacy.js
+++ visualizza-legacy.js
@@ -1355,7 +1355,6 @@ function calcolaSommaPrecedenteRiepilogo(tipo, sezione, annoPrec, mesePrec, grup
 }
 
 function applicaStiliCelle(data) {
-function applicaStiliCelle(data) {
   // Debug migliorato per vedere il contenuto effettivo
   console.log('Cella [' + data.row.index + ',' + data.column.index + ']:', 
               data.row.raw[0], 
