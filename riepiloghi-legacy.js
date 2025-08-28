6158f1cb50233b8b55e5550eec11b84109da2ccc
GerryPoz <pliplomail@gmail.com>
2025-08-28 15:38:20 +0200

    Update riepiloghi-legacy.js

--- riepiloghi-legacy.js
+++ riepiloghi-legacy.js
@@ -1,14 +1,4 @@
-// Configurazione Firebase (da firebase-config.js)
-var firebaseConfig = {
-  apiKey: "AIzaSyAGoVq0-DvyqUUH4aDCfOFZP2NobclIg_o",
-  authDomain: "hombu-8630c.firebaseapp.com",
-  databaseURL: "https://hombu-8630c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "hombu-8630c",
-  storageBucket: "hombu-8630c.firebasestorage.app",
-  messagingSenderId: "886418358212",
-  appId: "1:886418358212:web:fa87f614ad5665b3426e91",
-  measurementId: "G-D6W8XKCB8Z"
-};
+import { firebaseConfig } from './firebase-config.js';
 
 // Inizializza Firebase
 firebase.initializeApp(firebaseConfig);
