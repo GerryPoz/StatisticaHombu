a3ea1bbb4f7dc52da88e935f3554f7740ff8631a
GerryPoz <pliplomail@gmail.com>
2025-08-28 17:00:08 +0200

    Add files via upload

---
+++ firebase-config-legacy.js
@@ -0,0 +1,11 @@
+// Configurazione Firebase per versione legacy (compatibile con vecchi browser)
+var firebaseConfig = {
+  apiKey: "AIzaSyAGoVq0-DvyqUUH4aDCfOFZP2NobclIg_o",
+  authDomain: "hombu-8630c.firebaseapp.com",
+  databaseURL: "https://hombu-8630c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hombu-8630c",
+  storageBucket: "hombu-8630c.firebasestorage.app",
+  messagingSenderId: "886418358212",
+  appId: "1:886418358212:web:fa87f614ad5665b3426e91",
+  measurementId: "G-D6W8XKCB8Z"
+};
